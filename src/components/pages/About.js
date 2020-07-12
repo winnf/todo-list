59eb35e69a88888a6da5ee1c679ee8b5945ac79f
Winnie Fong <winnie_fon@hotmail.com>
2020-07-11 21:18:57 -0700

    links

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a TodoList app, part of a crash course</p>
+    </React.Fragment>
+  )
+}
+
+export default About
